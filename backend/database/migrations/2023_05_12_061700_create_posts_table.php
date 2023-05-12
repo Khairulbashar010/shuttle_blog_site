1bdf04d53ea0d4e16b856c93f6d1f6f937fde10e
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2023-05-12 20:15:33 +0600

    add migrations and repositories

---
+++ backend/database/migrations/2023_05_12_061700_create_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('blog_banner');
+            $table->text('blog_body');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
+
